a3be722368af408c392e3221498bf219ddfa1eef
Marii027 <mariestacioarquivos@gmail.com>
2025-06-27 19:20:21 +0000

    Adicionei a Densidade... e o PIB...

--- teste.c
+++ teste.c
@@ -9,6 +9,11 @@ int main(){
     float area1 = 331.354;
     float pib1 = 105.8;
     int pontosTuristicos1 = 25;
+    float densidadePopulacional1;
+    float pibPerCapita1;
+
+    densidadePopulacional1 = populacao1 / area1;
+    pibPerCapita1 = (pib1 * 1000000000) / populacao1;
 
     // Carta 2: Palmas (Tocantins)
     char estado2 = 'T';
@@ -18,6 +23,12 @@ int main(){
     float area2 = 2227.329;
     float pib2 = 10.3;
     int pontosTuristicos2 = 20;
+    float densidadePopulacional2;
+    float pibPerCapita2;
+    
+    densidadePopulacional2 = populacao2 / area2;
+    pibPerCapita2 = (pib2 * 1000000000) / populacao2;
+
 
     // Exibição da Carta 1
     printf("--- Carta 1 ---\n");
@@ -28,6 +39,9 @@ int main(){
     printf("Área: %.3f km²\n", area1);
     printf("PIB: R$ %.1f bilhões de reais\n", pib1);
     printf("Número de Pontos Turísticos: %d\n\n", pontosTuristicos1);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidadePopulacional1);
+    printf("PIB per Capita: %.2f reais\n\n", pibPerCapita1);
+
 
     // Exibição da Carta 2
     printf("--- Carta 2 ---\n");
@@ -38,6 +52,9 @@ int main(){
     printf("Área: %.3f km²\n", area2);
     printf("PIB: R$ %.1f bilhões de reais\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidadePopulacional2);
+    printf("PIB per Capita: %.2f reais\n", pibPerCapita2);
+
 
     return 0;
 }
